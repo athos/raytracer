6f05a121717614fed0721a1a8d3f53169488b5d8
OHTA Shogo <athos0220@gmail.com>
2017-12-28 13:17:16 +0900

    Implement first gradation sample

--- src/raytracer/core.cljs
+++ src/raytracer/core.cljs
@@ -1,17 +1,29 @@
 (ns raytracer.core
-    (:require ))
+  (:require [clojure.browser.dom :as dom]
+            [goog.events :as events]))
 
 (enable-console-print!)
 
-(println "This text is printed from src/raytracer/core.cljs. Go ahead and edit it and see reloading in action.")
+(def width 500)
+(def height 250)
 
-;; define your app data so that it doesn't get over-written on reload
+(defn main []
+  (let [canvas (dom/get-element :canvas)
+        ctx (.getContext canvas "2d")
+        pixels (.createImageData ctx width height)
+        data (.-data pixels)]
+    (doseq [i (range (dec height) -1 -1)
+            j (range width)
+            :let [pos (* (+ (* i width) j) 4)
+                  r (int (* 255.99 (/ i width)))
+                  g (int (* 255.99 (/ j height)))
+                  b (int (* 255.99 0.2))]]
+      (aset data (+ pos 0) r)
+      (aset data (+ pos 1) g)
+      (aset data (+ pos 2) b)
+      (aset data (+ pos 3) 255))
+    (.putImageData ctx pixels 0 0)))
 
-(defonce app-state (atom {:text "Hello world!"}))
+(events/listen js/window "load" main)
 
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn on-js-reload [])
