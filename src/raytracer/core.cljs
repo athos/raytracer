c0a9f579fdae6c778b0a0a35d64a184e6355ae58
OHTA Shogo <athos0220@gmail.com>
2017-12-29 20:26:10 +0900

    Refactor code

--- src/raytracer/core.cljs
+++ src/raytracer/core.cljs
@@ -1,32 +1,21 @@
 (ns raytracer.core
   (:require [clojure.browser.dom :as dom]
-            [goog.events :as events]
-            thinktopic.aljabr.core
             [clojure.core.matrix :as mat]
-            [clojure.core.matrix.operators :as ops]))
+            [clojure.core.matrix.operators :as ops]
+            [goog.events :as events]
+            [raytracer.hittable :as hit]
+            [raytracer.ray :as ray]
+            [raytracer.sphere :as sphere]
+            thinktopic.aljabr.core))
 
 (enable-console-print!)
 
 (def width 200)
 (def height 100)
 
-(defn point-at [{:keys [origin dir]} t]
-  (ops/+ origin (ops/* t dir)))
-
-(defn hit-sphere [center radius {:keys [origin dir]}]
-  (let [oc (ops/- origin center)
-        a (mat/dot dir dir)
-        b (* 2.0 (mat/dot oc dir))
-        c (- (mat/dot oc oc) (* radius radius))
-        discriminant (- (* b b) (* 4 a c))]
-    (when (>= discriminant 0)
-      (/ (- (- b) (Math/sqrt discriminant))
-         (* 2.0 a)))))
-
-(defn color [{:keys [dir] :as r}]
-  (if-let [t (hit-sphere [0 0 -1] 0.5 r)]
-    (let [N (mat/normalise (ops/- (point-at r t) [0 0 -1]))]
-      (ops/* 0.5 (ops/+ N 1)))
+(defn color [{:keys [dir] :as ray} world]
+  (if-let [rec (hit/hit world ray)]
+    (ops/* 0.5 (ops/+ (:normal rec) 1))
     (let [dir (mat/normalise dir)
           t (* 0.5 (+ (mat/select dir 1) 1.0))]
       (ops/+ (ops/* (- 1.0 t) [1.0 1.0 1.0])
@@ -40,17 +29,19 @@
         lower-left [-2.0 -1.0 -1.0]
         horizontal [4.0 0.0 0.0]
         vertical [0.0 2.0 0.0]
-        origin [0.0 0.0 0.0]]
+        origin [0.0 0.0 0.0]
+        world [(sphere/->Sphere [0 0 -1] 0.5)
+               (sphere/->Sphere [0 -100.5 -1] 100)]]
     (doseq [j (range (dec height) -1 -1)
             i (range width)
             :let [u (/ i width)
                   v (/ j height)
-                  r {:origin origin
-                     :dir (ops/+ lower-left
-                                 (ops/* u horizontal)
-                                 (ops/* v vertical))}
+                  r (ray/->Ray origin
+                               (ops/+ lower-left
+                                      (ops/* u horizontal)
+                                      (ops/* v vertical)))
                   pos (* (+ (* (- height j) width) i) 4)
-                  col (color r)]]
+                  col (color r world)]]
       (aset data (+ pos 0) (int (* 255.99 (mat/select col 0))))
       (aset data (+ pos 1) (int (* 255.99 (mat/select col 1))))
       (aset data (+ pos 2) (int (* 255.99 (mat/select col 2))))
